f111804e3cc9617fc0947b15ea3edff24d8766e4
kanaka <kumarappamadhu@gmail.com>
2026-02-13 11:07:54 +0530

    first task done

---
+++ user_login _check.py
@@ -0,0 +1,14 @@
+""" 1. User Login Check
+Problem Statement:
+Given a username and password, check whether login is successful.
+username = "admin"
+password = "1234"
+"""
+print("Enter the username :")
+username_input = input()
+print("Enter the password :")
+password_input = input()
+if username_input == "admin" and password_input == "1234":
+    print("Login successful.")
+else:
+    print("Login failed.")
